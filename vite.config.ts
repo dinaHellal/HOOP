bff152e172802c3d956806d5facd88fbfca0dd73
dina hellal <dinahellal12@gmail.com>
2025-08-06 23:56:51 +0200

    Add PWA support with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),     tailwindcss(),],
-  build: {
-  rollupOptions: {
-    input: {
-      main: 'index.html'
-    }
-  }
-}
+  plugins: [
+          react(),      tailwindcss(),
 
-})
+    VitePWA({
+
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Hoop Hijab Fashion",
+        short_name: "Hoop",
+        description: "Hijab Fashion Store",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icon512_rounded.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icon512_maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      }
+    })
+  ]
+});
+
