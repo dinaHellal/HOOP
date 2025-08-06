7c0b94d635c7d4c829382c4609c37d3e678b3a38
dina hellal <dinahellal12@gmail.com>
2025-08-06 22:59:34 +0200

    Delete: Service Worker File

--- src/main.tsx
+++ src/main.tsx
@@ -14,11 +14,4 @@ createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </StrictMode>
 );
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((reg) => console.log("Service Worker registered:", reg.scope))
-      .catch((err) => console.log("Service Worker registration failed:", err));
-  });
-}
+
